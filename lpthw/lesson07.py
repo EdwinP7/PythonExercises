35c04a53515830ffebb642af1b88a13e1bdec96c
Edwin P <Edwin P>
2017-02-06 21:07:39 -0800

    Lesson 7

---
+++ lpthw/lesson07.py
@@ -0,0 +1,37 @@
+# Prints a string
+print "Mary had a little lamb."
+# Prints a string and a formatted string
+print "Its fleece was white as %s" % 'snow'
+# Prints a string
+print "And everywhere that Mary went."
+# Prints the string '.' ten times
+print "." * 10
+# Initializes a variable
+end1 = "C"
+# Initializes a variable
+end2 = "h"
+# Initializes a variable
+end3 = "e"
+# Initializes a variable
+end4 = "e"
+# Initializes a variable
+end5 = "s"
+# Initializes a variable
+end6 = "e"
+# Initializes a variable
+end7 = "B"
+# Initializes a variable
+end8 = "u"
+# Initializes a variable
+end9 = "r"
+# Initializes a variable
+end10 = "g"
+# Initializes a variable
+end11 = "e"
+# Initializes a variable
+end12 = "r"
+# Prints out a sequence of strings with no space until last one,
+# where the comma says next printed will be on same line after a space
+print end1 + end2 + end3 + end4 + end5 + end6
+#
+print end7 + end8 + end9 + end10 + end11 + end12
