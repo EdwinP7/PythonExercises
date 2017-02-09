6fa3185bbff6abe93926abece6f40b5497db6d35
Edwin P <edwinperea7@gmail.com>
2017-02-09 12:32:54 -0800

    Additional lessons

---
+++ lpthw/lesson13.py
@@ -0,0 +1,9 @@
+# From the sys module/library, import the 'argv' class
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("The first variable is:", first)
+print("The second variable is:", second)
+print("The third variable is:", third)
