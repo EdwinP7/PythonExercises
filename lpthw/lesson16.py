b080836e57ee9034871baef58740410d0daa116b
Edwin P <edwinperea7@gmail.com>
2017-04-07 23:28:23 -0700

    imports

--- lpthw/lesson16.py
+++ lpthw/lesson16.py
@@ -1,3 +1,13 @@
+form sys import argv
+
+script, filename = argv
+
+print "We're going to erase %r." % filename
+print "If you don't want that, hit CTRL-C (^C)."
+print "If you do want that, hit ENTER/RETRUN."
+
+raw_input("?")
+
 print "Opening the file..."
 target = open(filename, 'w')
 
